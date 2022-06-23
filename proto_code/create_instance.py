d366658c8610a537d15a8eb8adb9de937d00a48d
gorpon <gorpon@yardbird>
2022-06-23 17:48:20 +0700

    lists all instance templates... actuall works after setting up gcloud auth yay

--- proto_code/create_instance.py
+++ proto_code/create_instance.py
@@ -1,5 +1,12 @@
-from google.cloud.compute_v1 import InstanceTemplate
-with open('templates/instance-test1') as fh:
-    tempdata = fh.read()
-mytemp = InstanceTemplate.from_json(tempdata, ignore_unknown_fields=True)
+#from google.cloud.compute_v1 import InstanceTemplate
+#with open('templates/instance-test1') as fh:
+#    tempdata = fh.read()
+#mytemp = InstanceTemplate.from_json(tempdata, ignore_unknown_fields=True)
 
+# lists all instance templates
+from googleapiclient.discovery import build
+service = build('compute', 'v1')
+instance_template = service.instanceTemplates()
+req = instance_template.list(project='dopest-project-ever-351102')
+response = req.execute()
+response
